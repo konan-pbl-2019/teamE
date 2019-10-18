3bbaffe6110210c14f36c363bc38a3cb19f8133f
s1871112 <s1871112@d-13102-098.ku-kinds.konan-u.ac.jp>
2019-10-18 14:46:46 +0900

    上下左右のkeyを直しました

--- src/template/quiz2D/QuizGameContainer.java
+++ src/template/quiz2D/QuizGameContainer.java
@@ -39,7 +39,7 @@ public class QuizGameContainer extends BaseScenarioGameContainer {
 
 		dialog.setRelativePosition(0.2f, 0.75f);	// ダイアログ
 		dialog.setFont(new Font("", Font.PLAIN, 12));	// 文字のフォント
-		dialog.setColor(Color.WHITE);				// 文字の色
+		dialog.setColor(Color.BLACK);				// 文字の色
 		addWidget(dialog);
 
 		Font f = new Font("", Font.PLAIN, 30);
@@ -70,8 +70,8 @@ public class QuizGameContainer extends BaseScenarioGameContainer {
 		optionButtons[3].setRelativeHeight(0.08f);
 		optionButtons[3].setRelativeWidth(0.2f);
 		addSelectableWidget(optionButtons[3], 1, 1);
-		 
-				
+
+
 		repaint();
 	}
 
